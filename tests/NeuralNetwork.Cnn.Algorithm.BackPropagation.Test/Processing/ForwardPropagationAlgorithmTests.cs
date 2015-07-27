621fef871537f5c14c157861b98774df4084fc1d
Popitich Alex <sasha.popitich@outlook.com>
2015-07-28 00:04:53 +0300

    Add tests for Forward ropagation algorithm

---
+++ tests/NeuralNetwork.Cnn.Algorithm.BackPropagation.Test/Processing/ForwardPropagationAlgorithmTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using DigitR.Core.InputProvider;
+using DigitR.Core.NeuralNetwork;
+using DigitR.NeuralNetwork.Cnn;
+using DigitR.NeuralNetwork.Cnn.Algorithms.Activation;
+using DigitR.NeuralNetwork.Cnn.Algorithms.Processing;
+using DigitR.NeuralNetwork.Cnn.ConnectionSchemes.Implementation;
+using DigitR.NeuralNetwork.Cnn.Primitives;
+
+using NeuralNetwork.Cnn.Test.Processing.Mock;
+
+using Xunit;
+
+namespace NeuralNetwork.Cnn.Test.Processing
+{
+    public class ForwardPropagationAlgorithmTests
+    {
+        private readonly INeuralNetworkBuilder<double> networkBuilder =
+           new NeuralNetworkBuilder<double>();
+
+        [Fact]
+        public void InputsSourceAndInputNeuronsNumbersMismatchTest()
+        {
+            IInputPattern<double[]> inputPattern = new InputPatternMock(new [] { 1.0, -1.0, 1.0 });
+
+            INeuralNetwork<double[]> neuralNetwork = networkBuilder
+                .AddInputLayer(new CnnLayer(0, inputPattern.Source.Length + 1, true, false))
+                .AddLayer<FullyConnectedScheme>(new CnnLayer(1, 2, false, true))
+                .Build<CnnNeuralNetworkFactory>();
+
+            var processingAlgorithm = new ForwardPropagationAlgorithm(new HyperbolicTgActivationAlgorithm());
+
+            Assert.NotNull(neuralNetwork);
+            Assert.Throws<ArgumentException>(() => processingAlgorithm.Process(neuralNetwork, inputPattern));
+        }
+    }
+}
